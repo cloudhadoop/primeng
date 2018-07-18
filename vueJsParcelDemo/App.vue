b4f25551032342ca3a844a09c65e9341964c6105
unknown <ikiranbabu@gmail.com>
2018-07-18 17:50:35 +0530

    commit changes

---
+++ vueJsParcelDemo/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="mainApp">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mainApp',
+  data () {
+    return {
+      msg: 'Parcel Hello World Application'
+    }
+  }
+}
+</script>
